cd431329e146ae9ec8f908ca8ed0b94e48115cf4
Jia Cheng Teo <jcteo.2018@scis.smu.edu.sg>
2022-01-20 18:20:08 +0800

    tutorial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
+  const name = "eli"
+  const age = 13
+  const canDrink = age >= 18
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,10 +19,12 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <h1>gg.com Hello {name} </h1>
+        <h2>Drinking eligibility: {canDrink ? "True" : "False"}</h2>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
